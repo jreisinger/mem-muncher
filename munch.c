d551030d28cbd770d583b643432a805cf5f0a64d
Jozef Reisinger <jozef.reisinger@telekom.com>
2018-04-04 13:21:45 +0200

    add step variable

--- munch.c
+++ munch.c
@@ -3,6 +3,8 @@
 #include <string.h>
 
 int main(int argc, char** argv) {
+    int step = 100; // increase this to have your memory munched faster :-)
+
     int max = -1;
     int mb = 0;
     char* buffer;
@@ -10,10 +12,10 @@ int main(int argc, char** argv) {
     if(argc > 1)
         max = atoi(argv[1]);
 
-    while((buffer=malloc(100*1024*1024)) != NULL && mb != max) {
-        memset(buffer, 0, 100*1024*1024);
+    while((buffer=malloc(step*1024*1024)) != NULL && mb != max) {
+        memset(buffer, 0, step*1024*1024);
         mb++;
-        printf("Allocated %d MB\n", 100*mb);
+        printf("Allocated %d MB\n", step*mb);
         sleep(1);
     }
 
